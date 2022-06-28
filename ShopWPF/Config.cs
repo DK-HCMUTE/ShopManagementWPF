fea680ca60f46e95a23aeaa031caef93c17d39d5
tuankiet2709 <tuankietlenguyen2709@gmail.com>
2022-06-28 13:38:11 +0700

    commit with branch 3tier

---
+++ ShopWPF/Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace ShopWPF
+{
+    public class Config
+    {
+        public static System.Data.SqlClient.SqlConnection getConnection()
+        {
+            try
+            {
+                string connectionStr = @"Data Source=LAPTOP-BIITLP3C\TUANKIET;Initial Catalog=AppBanHang;Integrated Security=True";
+                System.Data.SqlClient.SqlConnection conn = new SqlConnection(connectionStr);
+                return conn;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
